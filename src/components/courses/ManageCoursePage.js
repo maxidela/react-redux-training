b2fde1bb1e50d8134ac8812c5dce86ad1f2a388c
maxidela <maxidela@gmail.com>
2020-08-26 15:20:46 -0300

    Adding useEffect

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { loadCourses } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+
+const ManageCoursePage = ({ courses, authors, loadCourses, loadAuthors }) => {
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
+};
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
